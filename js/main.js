b0588da93d583348b954d4ffc3ba3f1276fc6aae
Jesus <jesus.zarate.eng@gmail.com>
2018-04-19 23:14:03 -0600

    added coors proxy

--- js/main.js
+++ js/main.js
@@ -2,13 +2,11 @@ Date.prototype.format = function (mask, utc) {
     return dateFormat(this, mask, utc);
 };
 
-// let _baseUrl = 'https://air.eng.utah.edu/dbapi/api/rawDataFrom';
-
 L.TimeDimension.Layer.SODAHeatMap = L.TimeDimension.Layer.extend({
 
     initialize: function (options) {
-        var heatmapCfg = this._getHeatmapOptions(options.heatmatOptions || {});
-        var layer = new HeatmapOverlay(heatmapCfg);
+        let heatmapCfg = this._getHeatmapOptions(options.heatmatOptions || {});
+        let layer = new HeatmapOverlay(heatmapCfg);
         L.TimeDimension.Layer.prototype.initialize.call(this, layer, options);
         this._currentLoadedTime = 0;
         this._currentTimeData = {
@@ -20,8 +18,8 @@ L.TimeDimension.Layer.SODAHeatMap = L.TimeDimension.Layer.extend({
     },
 
     _getHeatmapOptions: function (options) {
-        var config = {};
-        var defaultConfig = {
+        let config = {};
+        let defaultConfig = {
             radius: 10,
             maxOpacity: .8,
             scaleRadius: false,
@@ -30,10 +28,10 @@ L.TimeDimension.Layer.SODAHeatMap = L.TimeDimension.Layer.extend({
             lngField: 'lng',
             valueField: 'count'
         };
-        for (var attrname in defaultConfig) {
+        for (let attrname in defaultConfig) {
             config[attrname] = defaultConfig[attrname];
         }
-        for (var attrname in options) {
+        for (let attrname in options) {
             config[attrname] = options[attrname];
         }
         return config;
@@ -73,16 +71,21 @@ L.TimeDimension.Layer.SODAHeatMap = L.TimeDimension.Layer.extend({
 
     },
 
+    myJsonMethod: function (){
+        console.log('here')
+    },
+
     _makeRequest: function (url, time) {
-        var oReq = new XMLHttpRequest();
+
+        let oReq = new XMLHttpRequest();
         oReq.addEventListener("load", (function (xhr) {
 
             try {
-                var response = xhr.currentTarget.response;
-                var data = JSON.parse(response);
+                let response = xhr.currentTarget.response;
+                let data = JSON.parse(response);
 
-                for (var i = 0; i < data['data'].length && i <= 1; i++) {
-                    var marker = data['data'][i];
+                for (let i = 0; i < data['data'].length && i <= 1; i++) {
+                    let marker = data['data'][i];
 
                     this._currentTimeData.data.push({
                         lat: marker.Latitude,
@@ -110,34 +113,34 @@ L.TimeDimension.Layer.SODAHeatMap = L.TimeDimension.Layer.extend({
     },
 
     _constructQuery: function (id, time) {
-        var bbox = this._map.getBounds();
-        var sodaQueryBox = [bbox._northEast.lat, bbox._southWest.lng, bbox._southWest.lat, bbox._northEast.lng];
+        let bbox = this._map.getBounds();
+        let sodaQueryBox = [bbox._northEast.lat, bbox._southWest.lng, bbox._southWest.lat, bbox._northEast.lng];
 
-        var startDate = new Date(time);
-        var endDate = new Date(startDate.getTime());
+        let startDate = new Date(time);
+        let endDate = new Date(startDate.getTime());
         L.TimeDimension.Util.addTimeDuration(endDate, this._period, false);
 
-        var params = "?id=" + id +
+        let params = "?id=" + id +
             "&sensorSource=PurpleAir&" +
             "start=" + startDate.format('yyyy-mm-dd') + "T" + startDate.format('HH:MM:ss') + "Z&" +
             "end=" + endDate.format('yyyy-mm-dd') + "T" + endDate.format('HH:MM:ss') + "Z&" +
             "show=all";
-        var url = this._baseURL + params;
+        let url = "https://cors-anywhere.herokuapp.com/" + this._baseURL + params;
         console.log(url);
         return url;
     },
 
     _callAPI: function (time) {
-        var ids = [];
-        var _this = this;
+        let ids = [];
+        let _this = this;
 
         jQuery.get('data/PurpleAirIDs.txt', undefined, function (data) {
             ids = data.split('\n');
         }, "html").done(function () {
 
-            var i;
+            let i;
             for (i = 0; i < ids.length; i++) {
-                var url = _this._constructQuery(ids[i], time);
+                let url = _this._constructQuery(ids[i], time);
                 _this._makeRequest(url, time);
             }
         });
@@ -170,7 +173,6 @@ map.addLayer(layer);
 
 let testSODALayer = L.timeDimension.layer.sodaHeatMap({
     baseURL: 'https://air.eng.utah.edu/dbapi/api/rawDataFrom',
-    // baseUrl: _baseUrl,
 });
 testSODALayer.addTo(map);
 
@@ -189,34 +191,6 @@ let timeDimensionControl = new L.Control.TimeDimensionCustom({
 map.addControl(timeDimensionControl);
 
 let ids = [];
-// jQuery.get('data/PurpleAirTest.csv', undefined, function (data) {
-//     ids = data.split('\n');
-// }, "html").done(function (con) {
-//
-//     for (let i = 1; i < ids.length; i++) {
-//         let sensor = ids[i].split(',');
-//
-//         marker = L.circle(40.643894, -112.380188,
-//             // marker = L.circle(+sensor[1], +sensor[2],
-//             {
-//                 // color: '#00FFFF00',
-//                 color: 'red',
-//                 fillColor: '#00FFFF00',
-//                 fillOpacity: 0.5,
-//                 radius: 500
-//             })
-//             .bindPopup(sensor[0])
-//             .addTo(map);
-//
-//         marker.on('mouseover', function (e) {
-//             this.openPopup();
-//         });
-//
-//         marker.on('mouseout', function (e) {
-//             this.closePopup();
-//         });
-//     }
-// });
 
 jQuery.get('data/PurpleAirTest.csv', undefined, function (data) {
     ids = data.split('\n');
@@ -232,7 +206,7 @@ jQuery.get('data/PurpleAirTest.csv', undefined, function (data) {
                 fillOpacity: 0.5,
                 radius: 500
             })
-            .bindPopup("<b>Sensor: "+ sensor[0] + "</b><br>PM: " + sensor[3])
+            .bindPopup("<b>Sensor: " + sensor[0] + "</b><br>PM: " + sensor[3])
             .addTo(map);
 
         marker.on('mouseover', function (e) {
@@ -244,8 +218,3 @@ jQuery.get('data/PurpleAirTest.csv', undefined, function (data) {
         });
     }
 });
-
-
-// function f(sensor) {
-//
-// }
